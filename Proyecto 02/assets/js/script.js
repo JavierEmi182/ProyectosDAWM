31105229ea8704ffee5e1021c50761efe7b0d1bc
JavierEmi182 <javvespi@espol.edu.ec>
2022-11-17 13:24:54 -0500

    Aumentos para fetch, mejor manejo de llamadas y contadores

--- Proyecto 02/assets/js/script.js
+++ Proyecto 02/assets/js/script.js
@@ -1,80 +1,56 @@
 var fetchbtn= document.getElementById('fetch');
-fetchbtn.addEventListener('click', (event) => {
-      for (let i = 0; i < 3; i++) {
-            cargarDatos(i)
+      fetchbtn.addEventListener('click', (event) => {
+      let imgFetch= document.getElementsByClassName('imgFetch')
+      console.log(imgFetch)
+      for (let i = 0; i < imgFetch.length; i++) {
+            cargarDatos(imgFetch[i])
       }
       //mostrarDatos()
 });
 
-//window.addEventListener('DOMContentLoaded', (event) => {
-//      cargarDatos()
-      //mostrarDatos()
-//});
 
-let cargarDatos = (i) => {
+let cargarDatos = (imgFetch) => {
       let url = 'https://random.dog/woof.json'
       fetch(url)
       .then((response) => response.text())
       .then((data) => {
 
-            //////const obj = JSON.parse(data);
-            //////console.log(obj);
-
-            //data sacar url
-            //////const imgURL = obj.url
-            //////console.log(imgURL)
-
-            //setear en img (imgAns)
-            //let setear = 
-            //let img = document.createElement("img")
-            //img.setAttribute("src", imgURL)
-
-            //let select = document.getElementsByClassName('g')[0].innerHTML+=img
-
                   const obj = JSON.parse(data);
                   console.log(obj);
                   //data sacar url
                   const imgURL = obj.url
-                  console.log(imgURL)
-                  if(i==0){
-                        let select1 = document.getElementById('imgAns1')
-                        console.log(select1)
-                        select1.setAttribute("src",imgURL)
-                        let extension = imgURL.split('.')[2]
-                        console.log(extension)
-                        if(extension=='mp4'||extension=='.webm'){
-                              cargarDatos(i)
-                        }
-                  }else if(i==1){
-                        let select2 = document.getElementById('imgAns2')
-                        console.log(select2)
-                        select2.setAttribute("src",imgURL)
-                        let extension = imgURL.split('.')[2]
-                        console.log(extension)
-                        if(extension=='mp4'||extension=='.webm'){
-                              cargarDatos(i)
-                        }
-                  }else{
-                        let select3 = document.getElementById('imgAns3')
-                        console.log(select3)
-                        select3.setAttribute("src",imgURL)
-                        let extension = imgURL.split('.')[2]
-                        console.log(extension)
-                        if(extension=='mp4'||extension=='.webm'){
-                              cargarDatos(i)
-                        }
+                  console.log(imgURL) 
+                  imgFetch.setAttribute("src",imgURL)
+                  let extension = imgURL.split('.')[2].toUpperCase()
+                  console.log(extension)
+                  if(extension=='MP4'||extension=='WEBM'){
+                        cargarDatos(imgFetch)
+                        let cont = document.getElementById('contrec').innerHTML
+                        cont++;
+                        document.getElementById('contrec').innerHTML=cont
+                        return
                   }
-            
 
-            /*let select1 = document.getElementById('imgAns1')
-            console.log(select1)
-            select1.setAttribute("src",imgURL)
+                  if(extension=='JPG'){
+                        let cont = document.getElementById('contJPG').innerHTML
+                        cont++;
+                        document.getElementById('contJPG').innerHTML=cont
+                  } else if(extension=='PNG'){
+                        let cont = document.getElementById('contPNG').innerHTML
+                        cont++;
+                        document.getElementById('contPNG').innerHTML=cont
+                  }else if(extension=='GIF'){
+                        let cont = document.getElementById('contGIF').innerHTML
+                        cont++;
+                        document.getElementById('contGIF').innerHTML=cont
+                  }else{
+                        let cont = document.getElementById('contotros').innerHTML
+                        cont++;
+                        document.getElementById('contotros').innerHTML=cont
+                  }
 
-            let extension = imgURL.split('.')[2]
-            if(extension=='mp4'){
-                  cargarDatos()
-            }*/
-            //console.log(extension)
+                  //document.getElementById('cont JPG')[0].innerHTML = `` 
+
       })
 }
 
